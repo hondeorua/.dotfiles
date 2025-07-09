070ff21b548d9e07a015564c32a7a23e092fadde
hvu <hoang040507@gmail.com>
2025-07-09 22:13:07 +0700

    changed font size terminal

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,7 +12,7 @@ config.initial_cols = 95
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 16
+config.font_size = 19.5
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
 
 config.enable_tab_bar = false
