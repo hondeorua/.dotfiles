e86feba3a73eec8e2680f7b9191b5fbff254aeb8
hvu <hoang040507@gmail.com>
2025-07-23 11:07:27 +0700

    Add floaterminal and its keymap
    For easy testing of shiet i.e. leetcode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,7 @@ require 'config.options'
 require 'config.keymaps'
 require 'config.autocmds'
 require 'config.lazy'
+require 'custom.floaterminal'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
