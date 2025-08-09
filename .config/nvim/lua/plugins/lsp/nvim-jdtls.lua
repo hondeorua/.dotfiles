4100dbc7824e4dd04cb9c668a3af73d60ca071d9
hvu <hoang040507@gmail.com>
2025-08-09 19:42:07 +0700

    Add nvim-jdtls cuz java is a needy bitch

--- .config/nvim/lua/plugins/lsp/nvim-jdtls.lua
+++ .config/nvim/lua/plugins/lsp/nvim-jdtls.lua
@@ -1,3 +1,17 @@
 return {
   'mfussenegger/nvim-jdtls',
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'java',
+      callback = function(args)
+        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+        local workspace_dir = '/Users/hvu/.cache/jdtls/workspace/' .. project_name
+        local config = {
+          cmd = { '/Users/hvu/.local/share/nvim/mason/packages/jdtls/bin/jdtls', '-data', workspace_dir },
+          root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+        }
+        require('jdtls').start_or_attach(config)
+      end,
+    })
+  end,
 }
