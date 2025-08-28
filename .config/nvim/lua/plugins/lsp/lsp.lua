fc4350e98dc5ffe0e7fb1de3fbf1d3ed894e26ed
hvu <hoang040507@gmail.com>
2025-08-28 10:49:50 -0500

    watever im tired

--- .config/nvim/lua/plugins/lsp/lsp.lua
+++ .config/nvim/lua/plugins/lsp/lsp.lua
@@ -224,7 +224,7 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         pyright = {},
         -- rust_analyzer = {},
@@ -286,7 +286,6 @@ return {
         'eslint_d',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
       -- require('mason-lspconfig').setup {
       --   -- handlers = {
       --   --   function(server_name)
